5b7fd7a76f0b71ff79c31a74397cbdb663bbe449
olyesung <olyesung@gmail.com>
2021-12-14 00:06:41 +0900

    9.13 Redirecting Users

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -49,18 +49,23 @@ const CREATE_ACCOUNT_MUTATION = gql`
 function SignUp() {
   const navigate = useNavigate();
   const onCompleted = (data) => {
+    const { username, password } = getValues();
     const {
       createAccount: { ok, error },
     } = data;
     if (!ok) {
       return;
     }
-    navigate(routes.home);
+    navigate(routes.home, {
+      message: "Account created. Please log in.",
+      username,
+      password,
+    });
   };
   const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
     onCompleted,
   });
-  const { register, handleSubmit, formState } = useForm({
+  const { register, handleSubmit, formState, getValues } = useForm({
     mode: "onChange",
   });
   const onSubmitValid = (data) => {

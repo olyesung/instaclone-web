aca9773ef0df5331517db86cb882fb90288022bd
olyesung <olyesung@gmail.com>
2021-12-13 09:56:10 +0900

    9.9 Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Fragment } from "react";
-import { useReactiveVar } from "@apollo/client";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Routes,
-} from "react-router-dom";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import { darkModeVar, isLoggedInVar } from "./apollo";
+import { client, darkModeVar, isLoggedInVar } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 import SignUp from "./screens/SignUp";
@@ -20,25 +15,27 @@ function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <GlobalStyles />
-        <Router>
-          <Fragment>
-            <Routes>
-              <Route
-                path={routes.home}
-                exact
-                element={isLoggedIn ? <Home /> : <Login />}
-              />
-              {!isLoggedIn ? (
-                <Route path={routes.signUp} exact element={<SignUp />} />
-              ) : null}
-            </Routes>
-          </Fragment>
-        </Router>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <Router>
+            <Fragment>
+              <Routes>
+                <Route
+                  path={routes.home}
+                  exact
+                  element={isLoggedIn ? <Home /> : <Login />}
+                />
+                {!isLoggedIn ? (
+                  <Route path={routes.signUp} exact element={<SignUp />} />
+                ) : null}
+              </Routes>
+            </Fragment>
+          </Router>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
